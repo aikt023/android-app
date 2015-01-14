f709ead90b6c5bf1d33255d26af0e4d4b5f13e33
seugxj <seugxj@seugxj-PC>
2015-01-14 13:21:52 +0800

    aaa

---
+++ Testgit/src/ConstantString.java
@@ -0,0 +1,59 @@
+
+
+public class ConstantString {
+	
+	private static final String chinazPagesPath = "ChinazPages/";  
+    private static final String allChinaz500UrlsFile = "ChinazPages/urlsOfTop500.txt";  
+    private static final String failedUrlsOfChinazTop500 = "ChinazPages/failedUrlsOfTop500.txt";  
+    private static final String chianzTop500PagesPath = "Top500homepagesOfChinaz/";  
+    
+    private static final String alexaPagesPath = "AlexaPages/";  
+    private static final String allAlexa500UrlsFile = "AlexaPages/AlexaUrlsOfTop500.txt";  
+    private static final String failedUrlsOfAlexaTop500 = "AlexaPages/failedUrlsOfTop500.txt";  
+    private static final String alexaTop500PagesPath = "Top500homepagesOfAlexa/";
+    
+    private static final String resultPath = "resultData/";
+    private static final String chinazResultData = "resultData/chianzRD.txt";
+    private static final String alexaResultData = "resultData/alexaRD.txt";
+    
+  
+    
+    public static String getChinazPagesPath() {  
+        return chinazPagesPath;  
+    }  
+    public static String getAllChinaz500UrlsFile() {  
+        return allChinaz500UrlsFile;  
+    }  
+    public static String getFailedUrlsOfChinazTop500() {  
+        return failedUrlsOfChinazTop500;  
+    }  
+    public static String getChianzTop500PagesPath() {  
+        return chianzTop500PagesPath;  
+    }  
+    
+    
+    public static String getAlexaPagesPath() {  
+        return alexaPagesPath;  
+    }  
+    public static String getAllAlexa500UrlsFile() {  
+        return allAlexa500UrlsFile;  
+    }  
+    public static String getFailedUrlsOfAlexaTop500() {  
+        return failedUrlsOfAlexaTop500;  
+    }      
+    public static String getAlexaTop500PagesPath() {  
+        return alexaTop500PagesPath;  
+    }  
+    
+    
+    public static String getResultPath() {  
+        return resultPath;  
+    }  
+    public static String getChinazResultData() {  
+        return chinazResultData;  
+    }  
+    public static String getAlexaResultData() {  
+        return alexaResultData;  
+    }  
+ 
+}
